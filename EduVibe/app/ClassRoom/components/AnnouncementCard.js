ac5a1119323a1237f4add343d7c5ac359b47c99e
dhopegraphics <143608743+dhopegraphics@users.noreply.github.com>
2024-07-31 00:09:07 +0000

    cuser

--- EduVibe/app/ClassRoom/components/AnnouncementCard.js
+++ EduVibe/app/ClassRoom/components/AnnouncementCard.js
@@ -1,16 +1,44 @@
-import React from 'react';
+import React  , {useState , useEffect } from 'react';
 import { View, TextInput, StyleSheet } from 'react-native';
 import { Card, Avatar } from 'react-native-elements';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { getCurrentUser } from '../../../lib/appwrite';
 
 export default function AnnouncementCard() {
+  const [currentUser, setCurrentUser] = useState(null);
+
+  useEffect(() => {
+    const fetchCurrentUser = async () => {
+      try {
+        // Check if user data is stored in AsyncStorage
+        const storedUser = await AsyncStorage.getItem('currentUser');
+        if (storedUser) {
+          setCurrentUser(JSON.parse(storedUser));
+        } else {
+          // Fetch the current user if not found in AsyncStorage
+          const user = await getCurrentUser();
+          setCurrentUser(user);
+          // Save user data to AsyncStorage
+          await AsyncStorage.setItem('currentUser', JSON.stringify(user));
+        }
+      } catch (error) {
+        console.error("Failed to fetch current user:", error);
+      }
+    };
+
+    fetchCurrentUser();
+  }, []);
+
   return (
     <Card containerStyle={styles.card}>
       <View style={styles.cardContent}>
+      {currentUser && (
         <Avatar
           rounded
-          source={{ uri: 'https://via.placeholder.com/80' }}
+          source={{ uri:  currentUser.avatar  }}
           size="medium"
         />
+      )}
         <TextInput
           placeholder="Announce something to your class..."
           style={styles.input}
